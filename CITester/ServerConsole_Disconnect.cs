1d6161839eaeb3d2cfd2ae5041d0697a88ad2e14
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-12-04 16:54:02 +0100

    modern problems require modern solutions

---
+++ CITester/ServerConsole_Disconnect.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ServerConsole_Disconnect.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using HarmonyLib;
+using Mirror;
+using UnityEngine;
+
+namespace Mistaken.CITester
+{
+    [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.Disconnect), typeof(NetworkConnection), typeof(string))]
+    internal static class ServerConsole_Disconnect
+    {
+        private static bool Prefix(NetworkConnection conn, string message)
+        {
+            return false;
+        }
+    }
+}
