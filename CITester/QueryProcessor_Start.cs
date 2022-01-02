cf300f7a0f76bd7fd66a3f405b81bc7cf9325707
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-01-02 14:39:04 +0100

    added QueryProcessor_Start patch

---
+++ CITester/QueryProcessor_Start.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="QueryProcessor_Start.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using HarmonyLib;
+using Mirror;
+using RemoteAdmin;
+
+namespace Mistaken.CITester
+{
+    [HarmonyPatch(typeof(QueryProcessor), nameof(QueryProcessor.Start))]
+    internal static class QueryProcessor_Start
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
